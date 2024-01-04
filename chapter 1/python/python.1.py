b5859cafbce518d47ec90a30b3fb2b6f96024b95
kimhanseop <fghjk000@naver.com>
2024-01-04 22:50:34 +0900

    Changes

---
+++ chapter 1/python/python.1.py
@@ -0,0 +1,14 @@
+print("Hello world")
+
+a = 10
+b = 1.1
+c = 'abc'
+d = 'a'
+e = True or False
+f = a + b
+n = None
+print(f"a: {a}, b: {b}, c: {c}, d: {d}, e; {e}")
+print(f"f: {f}")
+
+c = d + str(a)
+print((type (a)))
